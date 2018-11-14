af61cad66369faa4ab0c2a5f0d3a15113337ab09
Angelika Ziolkowska <angelikaenquiries@gmail.com>
2018-11-14 18:02:32 +0000

    Add files via upload

---
+++ PacmanCppProject.cpp
@@ -0,0 +1,7 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#include "PacmanCppProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PacmanCppProject, "PacmanCppProject" );
+ 
---
+++ PacmanCppProjectGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PacmanCppProjectGameMode.generated.h"
+
+UCLASS(minimalapi)
+class APacmanCppProjectGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	APacmanCppProjectGameMode();
+};
+
+
+
